8a6f389d41078bd223fcbe3ba45eefb752911a71
alexandre i w <alexandreinaciowatanabe@gmail.com>
2024-05-02 16:40:58 -0300

    chore: add primary and background colors and font-family orbitron

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,22 @@
-import type { Metadata } from 'next'
-import { Chakra_Petch } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next"
+import { Chakra_Petch, Orbitron } from "next/font/google"
+import "./globals.css"
 
 const chakraPetch = Chakra_Petch({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-chakra-petch",
+})
+
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-orbitron",
 })
 
 export const metadata: Metadata = {
-  title: 'Aiw Digital | Transformando ideias em realidade digital',
-  description: 'Transformando ideias em realidade digital',
+  title: "Aiw Digital | Transformando ideias em realidade digital",
+  description: "Transformando ideias em realidade digital",
 }
 
 export default function RootLayout({
@@ -18,7 +25,10 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-br">
+    <html
+      lang="pt-br"
+      className={`${chakraPetch.variable} ${orbitron.variable}`}
+    >
       <body className={chakraPetch.className}>{children}</body>
     </html>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,23 @@
-import type { Config } from 'tailwindcss'
+import { Orbit, Orbitron } from "next/font/google"
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsla(250,66%,75%,1)",
+        background: " hsla(253,27%,6%,1)",
+      },
+      fontFamily: {
+        sans: ["var(--font-chakra-pertch)", "sans-serif"],
+        orbitron: ["var(--font-orbitron)", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
